f8b5a68a059e00d80f984ef151d4bf8fd1e0a046
MantasSidabras <sid.mantas@gmail.com>
2024-03-24 15:29:52 +0200

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mantassidabras.github.io',
+	base: 'personal-blog',
 	integrations: [mdx(), sitemap()],
 });
