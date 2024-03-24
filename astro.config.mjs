6e5d8d366b91dd387f76babb135b39fd78a7aede
MantasSidabras <sid.mantas@gmail.com>
2024-03-24 16:14:21 +0200

    added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mantassidabras.github.io',
-	// base: 'personal-blog',
-	integrations: [mdx(), sitemap()],
+  site: 'https://mantassidabras.github.io',
+  // base: 'personal-blog',
+  integrations: [mdx(), sitemap(), tailwind()]
 });
